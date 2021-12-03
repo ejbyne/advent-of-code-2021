ebc9fdd2ecc1c17154e3fde3b3e65342082c90bd
Edward Byne <edward.byne@codecentric.de>
2021-12-03 22:06:02 +0100

    Day 3 part 2

--- src/day3.test.ts
+++ src/day3.test.ts
@@ -1,6 +1,8 @@
 import {
+  calculateCO2ScrubberRating,
   calculateEpsilonRate,
   calculateGammaRate,
+  calculateOxygenGeneratorRating,
   multiplyRatesAsDecimals,
 } from "./day3";
 import { readFile } from "fs/promises";
@@ -39,6 +41,7 @@ describe("Day 3", () => {
 10000
 11001
 00010
+01010
 `;
       const result = calculateGammaRate(input);
       expect(result).toBe("10110");
@@ -70,5 +73,69 @@ describe("Day 3", () => {
 
       expect(puzzleResult).toBe(3309596);
     });
+  });
+
+  describe("Part 2", () => {
+    it("calculates the oxygen generator rating", () => {
+      const input = `
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+`;
+
+      const result = calculateOxygenGeneratorRating(input);
+
+      expect(result).toBe("10111");
+    });
+
+    it("calculates the CO2 scrubber rating", () => {
+      const input = `
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+`;
+
+      const result = calculateCO2ScrubberRating(input);
+
+      expect(result).toBe("01010");
+    });
+  });
+
+  it("multiplies the oxygen generator rating and CO2 scrubber rating in decimal format", () => {
+    const result = multiplyRatesAsDecimals("10111", "01010");
+
+    expect(result).toBe(230);
+  });
+
+  it("calculates the puzzle result", async () => {
+    const input = await readFile("src/day3.input.txt", "utf-8"); //?
+
+    const oxygenGeneratorRating = calculateOxygenGeneratorRating(input);
+    const co2ScrubberRating = calculateCO2ScrubberRating(input);
+
+    const result = multiplyRatesAsDecimals(
+      oxygenGeneratorRating,
+      co2ScrubberRating
+    );
+
+    expect(result).toBe(2981085);
   });
 });
--- src/day3.ts
+++ src/day3.ts
@@ -1,18 +1,27 @@
 export function calculateGammaRate(input: string) {
-  const bits = input.trim().split("\n");
-  const firstBitRow = bits[0];
+  const bits = parseBits(input);
 
-  return firstBitRow.split("").reduce((gammaAggregate, _, index) => {
-    const bitsForColumn = bits.reduce((gamma, bitRow) => {
-      return [...gamma, bitRow[index]];
-    }, []);
-    const numberOfZeros = bitsForColumn.sort().findIndex((bit) => bit === "1");
-    const numberOfOnes = bitsForColumn.length - numberOfZeros;
+  return [...Array(bits[0].length)].reduce((gammaAggregate, _, index) => {
+    const { numberOfZeros, numberOfOnes } =
+      calculateNumberOfZerosAndOnesForColumn(bits, index);
 
     return numberOfZeros > numberOfOnes
       ? gammaAggregate + "0"
       : gammaAggregate + "1";
   }, "");
+}
+
+function parseBits(input: string) {
+  return input.trim().split("\n");
+}
+
+function calculateNumberOfZerosAndOnesForColumn(bits: string[], index: number) {
+  const bitsForColumn = bits.reduce((gamma, bitRow) => {
+    return [...gamma, bitRow[index]];
+  }, []);
+  const numberOfZeros = bitsForColumn.sort().findIndex((bit) => bit === "1");
+  const numberOfOnes = bitsForColumn.length - numberOfZeros;
+  return { numberOfZeros, numberOfOnes };
 }
 
 export function calculateEpsilonRate(gammaRate: string) {
@@ -23,5 +32,68 @@ export function calculateEpsilonRate(gammaRate: string) {
 }
 
 export function multiplyRatesAsDecimals(firstRate: string, secondRate: string) {
-  return parseInt(firstRate, 2) * parseInt(secondRate, 2);
+  return convertBitsToDecimal(firstRate) * convertBitsToDecimal(secondRate);
+}
+
+function convertBitsToDecimal(firstRate: string) {
+  return parseInt(firstRate, 2);
+}
+
+export function calculateOxygenGeneratorRating(input: string) {
+  const bits = parseBits(input);
+  const filterPredicate = filterMostCommon;
+
+  return filterByColumnRecursively(bits, 0, filterPredicate);
+}
+
+export function calculateCO2ScrubberRating(input: string) {
+  const bits = parseBits(input);
+  const filterPredicate = filterLeastCommon;
+
+  return filterByColumnRecursively(bits, 0, filterPredicate);
+}
+
+function filterByColumnRecursively(
+  bits: string[],
+  index: number,
+  filterPredicate: (
+    numberOfZeros: number,
+    numberOfOnes: number,
+    index: number
+  ) => (bitRow: string) => boolean
+) {
+  const { numberOfZeros, numberOfOnes } =
+    calculateNumberOfZerosAndOnesForColumn(bits, index);
+
+  const filteredBits = bits.filter(
+    filterPredicate(numberOfZeros, numberOfOnes, index)
+  );
+
+  if (filteredBits.length === 1) {
+    return filteredBits[0];
+  }
+
+  return filterByColumnRecursively(filteredBits, index + 1, filterPredicate);
+}
+
+function filterMostCommon(
+  numberOfZeros: number,
+  numberOfOnes: number,
+  index: number
+) {
+  return (bitRow) =>
+    numberOfZeros > numberOfOnes
+      ? bitRow[index] === "0"
+      : bitRow[index] === "1";
+}
+
+function filterLeastCommon(
+  numberOfZeros: number,
+  numberOfOnes: number,
+  index: number
+) {
+  return (bitRow) =>
+    numberOfOnes < numberOfZeros
+      ? bitRow[index] === "1"
+      : bitRow[index] === "0";
 }
